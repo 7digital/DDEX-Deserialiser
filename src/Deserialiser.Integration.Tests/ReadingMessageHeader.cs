53e36b02cd6082c9b31cd453373644bd53612351
Iain Ballard <iain.ballard@7digital.com>
2012-06-26 13:05:00 +0100

    A few more tests

--- src/Deserialiser.Integration.Tests/ReadingMessageHeader.cs
+++ src/Deserialiser.Integration.Tests/ReadingMessageHeader.cs
@@ -5,11 +5,34 @@ namespace Deserialiser.Unit.Tests
 	[TestFixture]
 	public class ReadingMessageHeader : BigDdexXml
 	{
-
 		[Test]
-		public void MessageThreadIdIsReadCorrectly()
+		public void Message_thread_id_is_read_correctly()
 		{
 			ddex.root.MessageHeader.MessageThreadId.should_be_equal_to("MyMessageThreadId");
+		}
+
+		[Test]
+		public void Message_id_should_be_read_correctly ()
+		{
+			ddex.root.MessageHeader.MessageId.should_be_equal_to("MyMessageId");
+		}
+
+		[Test]
+		public void Message_should_be_an_update_type ()
+		{
+			ddex.root.UpdateIndicator.should_be_equal_to("UpdateMessage");
+		}
+
+		[Test]
+		public void Message_control_type_should_be_Test_Message ()
+		{
+			ddex.root.MessageHeader.MessageControlType.should_be_equal_to("TestMessage");
+		}
+
+		[Test]
+		public void Message_sender_should_be_read_correctly ()
+		{
+			ddex.root.MessageHeader.MessageSender.PartyName.FullName.should_be_equal_to("ICanHaz Music Group");
 		}
 	}
 }
---
+++ src/Deserialiser.Integration.Tests/ResourceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Deserialiser.Unit.Tests
+{
+	[TestFixture]
+	public class ResourceTests : BigDdexXml
+	{
+		[Test]
+		public void Resources_should_have_2_sound_recordings()
+		{
+			ddex.root.ResourceList.SoundRecordings.Count.should_be_equal_to(2);
+		}
+
+		[Test]
+		public void Resources_should_have_1_image()
+		{
+			ddex.root.ResourceList.Images.Count.should_be_equal_to(1);
+		}
+
+		[Test]
+		public void Resources_should_have_no_videos()
+		{
+			ddex.root.ResourceList.Videos.Count.should_be_equal_to(0);
+		}
+	}
+}
