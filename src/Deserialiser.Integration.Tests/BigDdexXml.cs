5611cf520803aef183b38ab76f25c36ac0107512
wojtek <wojciech.kotlarski@7digital.com>
2012-06-26 18:01:29 +0100

    added files to example xml added simple image test

--- src/Deserialiser.Integration.Tests/BigDdexXml.cs
+++ src/Deserialiser.Integration.Tests/BigDdexXml.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DDEX_Deserialiser;
+﻿using DDEX_Deserialiser;
 using NUnit.Framework;
 
 namespace Deserialiser.Unit.Tests
 {
-	[TestFixture]
 	public class BigDdexXml
 	{
 		public DDEX ddex;
