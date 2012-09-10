a273a2f4d91e68aacbd7b610df762a1a585ea375
wojtek <wojciech.kotlarski@7digital.com>
2012-09-10 11:27:02 +0100

    give possibility to validate xml against schema during load of DDEX

--- src/Deserialiser.Integration.Tests/DDEXConstructionTests.cs
+++ src/Deserialiser.Integration.Tests/DDEXConstructionTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Xml.Linq;
+using System.Xml.Schema;
 using DDEX_Deserialiser;
 using NUnit.Framework;
 
@@ -21,6 +23,64 @@ namespace Deserialiser.Unit.Tests
 			{
 				var ddex = new DDEX(fileStream);
 				Assert.That(ddex, Is.Not.Null);
+			}
+		}
+
+		[Test]
+		public void Should_create_a_DDEX_from_XDocument()
+		{
+			XDocument document = XDocument.Load("artifacts/ddex-sample.xml");
+			var ddex = new DDEX(document);
+			Assert.That(ddex, Is.Not.Null);
+		}
+
+		[Test]
+		public void Should_create_and_validate_DDEX_using_stream()
+		{
+			using (var fileStream = new FileStream("artifacts/ddex-sample.xml", FileMode.Open))
+			{
+				var ddex = new DDEX(fileStream, true);
+				Assert.That(ddex, Is.Not.Null);
+			}
+		}
+
+		[Test]
+		public void Should_create_and_validate_DDEX_using_file_path()
+		{
+			var ddex = new DDEX("artifacts/ddex-sample.xml", true);
+			Assert.That(ddex, Is.Not.Null);
+		}
+
+		[Test]
+		public void Should_create_and_validate_DDEX_using_XDocument()
+		{
+			XDocument document = XDocument.Load("artifacts/ddex-sample.xml");
+			var ddex = new DDEX(document, true);
+			Assert.That(ddex, Is.Not.Null);
+		}
+
+		[Test]
+		public void Should_throw_exception_if_DDEX_validation_fails_using_XDocument()
+		{
+			XDocument document = XDocument.Load("artifacts/Incorrect_DDEX.xml");
+			var ex = Assert.Throws<XmlSchemaValidationException>(() => new DDEX(document, true));
+			Assert.That(ex.Message, Is.StringContaining("The element 'Release' has invalid child element 'ReleaseReference'. List of possible elements expected: 'ReleaseId'"));
+		}
+
+		[Test]
+		public void Should_throw_exception_if_DDEX_validation_fails_using_file_path()
+		{
+			var ex = Assert.Throws<XmlSchemaValidationException>(() => new DDEX("artifacts/Incorrect_DDEX.xml", true));
+			Assert.That(ex.Message, Is.StringContaining("The element 'Release' has invalid child element 'ReleaseReference'. List of possible elements expected: 'ReleaseId'"));
+		}
+
+		[Test]
+		public void Should_throw_exception_if_DDEX_validation_fails_using_stream()
+		{
+			using (var fileStream = new FileStream("artifacts/Incorrect_DDEX.xml", FileMode.Open))
+			{
+				var ex = Assert.Throws<XmlSchemaValidationException>(() => new DDEX(fileStream, true));
+				Assert.That(ex.Message, Is.StringContaining("The element 'Release' has invalid child element 'ReleaseReference'. List of possible elements expected: 'ReleaseId'"));
 			}
 		}
 	}
