448e2cef6eeb17a20d6baacf2ac96a8d8102dcff
wojtek <wojciech.kotlarski@7digital.com>
2012-10-19 13:35:45 +0100

    regenerated DDEX to map xs:date and xs:dateTime to DateTime? type instead of string

---
+++ src/Deserialiser.Integration.Tests/ValuesParseTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Deserialiser.Unit.Tests
+{
+	[TestFixture]
+	public class ValuesParseTests : BigDdexXml
+	{
+		[Test]
+		public void Should_date_time_be_parsed_properly()
+		{
+			var release = ddex.Root.ReleaseList.Releases.First();
+			var releaseDetailsByTerritory = release.ReleaseDetailsByTerritorys.First();
+			releaseDetailsByTerritory.OriginalReleaseDate
+				.Value.should_be_equal_to(new DateTime(2007, 08, 28));
+		}
+	}
+}
